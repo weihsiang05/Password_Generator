4e2f7c5157af1be9048e9fae65b8d50744bc4ce7
weihsiang05 <weihsiang0000@gmail.ocm>
2023-09-06 16:31:38 -0400

    use express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
+
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-  res.send('express app for RandomPassord page')
+  res.redirect('/password')
+})
+
+app.get('/password', (req, res) => {
+  res.render('index')
 })
 
 app.listen(port, () => {
